872e792aa5b43df9dc5f3d12f1c6dd5e42f121a4
Spencer Scorcelletti <asimpletune@users.noreply.github.com>
2017-04-28 13:34:50 -0700

    Add set membership operators, positive and negative

--- operator.swift
+++ operator.swift
@@ -1,5 +1,10 @@
 prefix operator |
 postfix operator |
+infix operator ∈
+infix operator ∋
+infix operator ∉
+infix operator ∌
+
 struct CollectionCount<C> where C: Collection {
     let count: C.IndexDistance
     init(_ value: C.IndexDistance) {
@@ -13,4 +18,19 @@ extension Collection {
     static postfix func | (operand: Self) -> CollectionCount<Self> {
       return CollectionCount(operand.count)
     }
+}
+
+extension Set {
+  static func ∈ (element: Element, aSet: Set) -> Bool {
+    return aSet.contains(element)
+  }
+  static func ∋ (aSet: Set, element: Element) -> Bool {
+    return aSet.contains(element)
+  }
+  static func ∉ (element: Element, aSet: Set) -> Bool {
+    return !aSet.contains(element)
+  }
+  static func ∌ (aSet: Set, element: Element) -> Bool {
+    return !aSet.contains(element)
+  }
 }
